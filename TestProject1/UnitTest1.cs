884ea29d4914146a3253f0b7d3adf10a4c5ee555
Ty Augustine <tyronea_2000@yahoo.com>
2024-03-23 10:20:09 -0400

    feat: added RAG with OpenSearch

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace TestProject1
         [Test]
         public Task Test1()
         {
-            var llm = Constants.GetChatModelTypeById("ai21.j2-mid-v1");
+            var llm = Constants.GetModelTypeById<IChatModel>("ai21.j2-mid-v1");
             var result = llm.GenerateAsync("Hello World", new ChatSettings());
             Console.WriteLine(result.Result);
             return Task.CompletedTask;
